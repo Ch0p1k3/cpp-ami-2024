c560cc699d993510621f7926149723cd7eb30f7b
Ch0p1k3 <ch00p.228@gmail.com>
2025-02-19 06:59:12 +0000

    sandbox

---
+++ sem11-12/sandbox/cmp.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <tuple>
+#include <vector>
+
+enum class Grade : uint8_t {
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+};
+
+struct Student {
+    std::string name;
+    std::string surname;
+    Grade grade;
+};
+
+
+std::ostream& operator<<(std::ostream& out, const Student& student) {
+    out << student.name << " " << student.surname << ' ' << static_cast<int>(student.grade);
+    return out;
+}
+
+
+bool operator<(const Student& left, const Student& right) {
+    return std::tie(right.grade, left.name, left.surname) < std::tie(left.grade, right.name, right.surname);
+}
+
+int main() {
+    std::vector<Student> students = {
+        {"Chil", "Guy", Grade::Three},
+        {"Ivan", "Luchsh", Grade::Five},
+        {"Maxim", "Bassalaev", Grade::Two},
+    };
+
+    std::sort(students.begin(), students.end());
+
+    for (const auto& student : students) {
+        std::cout << student << std::endl;
+    }
+}
