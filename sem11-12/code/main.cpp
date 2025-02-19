f6927bc1f27a35522632c5d9bb24453a8fe30be1
Ch0p1k3 <ch00p.228@gmail.com>
2025-02-19 09:14:45 +0000

    update

--- sem11-12/code/main.cpp
+++ sem11-12/code/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
+#define если if
+
+#define то {
+
+#define конец }
+
+#define печать(X) std::cout<<X<<std::endl;
+
+#ifndef TEST_FLAG
+
 int Sq(int i);
 
-int main() {
+#else
+
+long long Sq(long long i);
+
+#endif
+
+int32_t main() {
     std::cout << Sq(2) << std::endl;
+
+    если (0 == 0) то печать("Да") конец
 }
