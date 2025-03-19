7acc4f941a77687e7d1cbc89265321117c32eacb
Ch0p1k3 <ch00p.228@gmail.com>
2025-03-19 06:38:35 +0000

    update

--- ranges/1.cpp
+++ ranges/1.cpp
@@ -2,11 +2,10 @@
 #include <vector>
 
 int main() {
-    auto square = [](int n) { return n * n; };
     std::vector<int> v = {1, 2, 3, 4, 5};
     for (int e : v) {
         if (e % 2 == 0) {
-            std::cout << square(e) << std::endl;
+            std::cout << e * e << std::endl;
         }
     }
 }
